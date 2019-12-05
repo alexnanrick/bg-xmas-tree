24b5cd61fd4cce73c90c130e85e4c4709d951749
alexnanrick <alex.nanrick@gmail.com>
2019-12-05 22:53:15 +0000

    Add project setup files

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+# app/__init__.py
+
+from gpiozero import LEDBoard
+from gpiozero.tools import random_values
+from signal import pause
+
+
+def run_xmas_tree():
+    tree = LEDBoard(*range(2, 28), pwm=True)
+    for led in tree:
+        led.source_delay = 0.1
+        led.source = random_values()
+    pause()
